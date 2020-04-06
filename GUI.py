6f2157001026abec76f900f35941099dbc9fbc62
djx007 <1244316586@qq.com>
2020-04-06 10:17:06 +0800

    GUI

--- GUI.py
+++ GUI.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 import linecache
-#import logistic_class as logis
 
 class GUI():
-    def __init__(self, num, grads):
+    global grades
+    grades = {}
+    global question_num
+    question_num=1
+    def __init__(self, num):
         self.num = num
         self.window = tk.Tk()
         self.window.title("性格测试")  # 窗口名
-        self.grads=grads
+        self.first_question = linecache.getline('./question', 1)
         width = 600
         height = 400
         screenwidth = self.window.winfo_screenwidth()
@@ -22,32 +25,31 @@ class GUI():
         elif next == 1:
             self.num += 1
         line = linecache.getline('./question', self.num)
-        #print(self.num)
         return line
 
     def show_text(self, question):
-        self.text.delete(0.0,"end")
+        self.text.delete(0.0, "end")
         self.text.insert(tk.INSERT, question)
         self.text.pack()
 
     def click_radio0(self):
-        self.grads[1]=0
-        print(self.grads)
+        grades[question_num] = 0
+        print(grades)
+
     def click_radio1(self):
-
-        print(self.grads)
-
+        grades[question_num] = 1
+        #print(grades)
     def click_radio2(self):
-
-        print(self.grads)
+        grades[question_num] = 2
+        #print(grades)
 
     def click_radio3(self):
-
-        print(self.grads)
+        grades[question_num] = 3
+        #print(grades)
 
     def click_radio4(self):
-
-        print(self.grads)
+        grades[question_num] = 4
+        #print(grades)
 
     def radio(self):
         r = tk.IntVar()
@@ -64,26 +66,184 @@ class GUI():
         self.next_question_button = tk.Button(self.window, text="下一题", command=self.show_next_question)
         self.next_question_button.pack()
 
+
+
     def draw_window(self):
         self.text = tk.Text(self.window, width=60, height=4)
-        self.show_text("start test")
+        #self.show_text('start test')
+        self.show_text(linecache.getline('./question', 1))
         self.radio()
         self.button()
 
+
+
     def show_next_question(self):
-        question_text = self.choose_question(0, 1)
-        self.show_text(question_text)
+        if question_num not in grades.keys():
+            warring_window = tk.Tk()
+            warring_window.title("提示")
+            warring_text = tk.Label(warring_window, text="请点击上一题,回答第"+str(question_num)+"题")
+            width = 300
+            height = 150
+            screenwidth = warring_window.winfo_screenwidth()
+            screenheight = warring_window.winfo_screenheight()
+            alignstr1 = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+            warring_window.geometry(alignstr1)  # 窗口大小
+            warring_text.pack()
+            #grades[self.question_num]=-1  #错过某题没有回答，该题答案为-1，表示异常
+            #print("请点击上一题,回答第"+str(self.question_num)+"题")
+        else:
+            question_num += 1
 
     def show_prev_question(self):
         question_text = self.choose_question(1, 0)
+        question_num -= 1
         self.show_text(question_text)
+
+
 
     def run(self):
         self.draw_window()
         self.window.mainloop()
 
+
+
+
+
+class Score_caculate():
+    # 计算反向分数
+    def alculate_reverse_score(self, file_re_index):
+        re_index = []
+        for index in file_re_index:
+            re_index.append(int(index))
+        for i in re_index:
+            grades[i] = 4-grades[i]
+
+    # 得到每个大维度下的小维度分数之和
+    def slip_sum_grades(self, file_every_index):
+        all_index = []
+        every_6_index = []
+        f8_grade = []
+        f8_sum_list = []
+        for f in file_every_index:
+            all_index.append(f)
+
+        for m in range(5):
+            every_6_index.append(all_index[1 + (m * 12):12 + (m * 12):2])
+
+            for z in range(6):
+
+                string_ever_8 = every_6_index[m][z].split(sep='.')
+
+                for i in range(len(string_ever_8)):
+                    index = int(string_ever_8[i])
+                    f8_grade.append(grades.get(index))
+                f8_sum = sum(f8_grade)
+                f8_sum_list.append(f8_sum)
+
+                del f8_grade[:]
+        return f8_sum_list
+
+    # 得到全部的分数[N,E,O,A,C,N1~N6,E1~E6,…………]
+    def add_all_grades(self, f8_sum_list):
+        N = sum(f8_sum_list[0:6])
+        E = sum(f8_sum_list[6:12])
+        O = sum(f8_sum_list[12:18])
+        A = sum(f8_sum_list[18:24])
+        C = sum(f8_sum_list[24:30])
+        all_grades = [N, E, O, A, C]
+        all_grades.extend(f8_sum_list)
+        return all_grades
+
+    # 根据计算法则得出最后的分数
+    def calculation_really_grades(self,all_grades):
+        man_M = [83.78, 109.96, 110.64, 113, 123.88, 14.15, 13.82, 14.37, 15.35, 14.86, 11.23,
+                 21.53, 18.69, 15.1, 18.33, 17.24, 19.07, 15.19,
+                 20.7, 19.47, 15.02, 19.62, 20.64, 21.07, 19.03, 20.99, 15.33, 16.67, 19.91,
+                 20.8, 18.39, 23.47, 20.02, 20.48, 20.72]
+
+        man_SD = [24.41, 18.27, 15.88, 14.9, 20.32, 5.7, 5.24, 5.7, 4.73, 4.88,
+                  5.09, 4.27, 4.65, 4.37, 4.74, 4.64, 4.85,
+                  4.19, 4.88, 3.95, 3.61, 4.98, 3.97,
+                  3.94, 5.11, 3.53, 4.64, 4.06, 3.59,
+                  4.34, 4.21, 4.22, 4.34, 4.66, 4.63]
+        really_grade = []
+        f8_sum_list = all_grades
+        for i in range(35):
+
+            if f8_sum_list[i] < man_M[i] - 2 * man_SD[i]:
+                really_grade.append(1)
+            elif (f8_sum_list[i] >= man_M[i] - 2 * man_SD[i]) and (f8_sum_list[i] <= man_M[i] - 1.5 * man_SD[i]):
+                really_grade.append(2)
+            elif ((f8_sum_list[i] > man_M[i] - 1.5 * man_SD[i])) and (f8_sum_list[i] <= man_M[i] - man_SD[i]):
+                really_grade.append(3)
+            elif (f8_sum_list[i] > man_M[i] - man_SD[i]) and (f8_sum_list[i] <= man_M[i] - 0.5 * man_SD[i]):
+                really_grade.append(4)
+            elif (f8_sum_list[i] > man_M[i] - 0.5 * man_SD[i]) and (f8_sum_list[i] <= man_M[i]):
+                really_grade.append(5)
+            elif (f8_sum_list[i] > man_M[i]) and (f8_sum_list[i] <= man_M[i] + 0.3 * man_SD[i]):
+                really_grade.append(6)
+            elif (f8_sum_list[i] > man_M[i] + 0.3 * man_SD[i]) and (f8_sum_list[i] <= man_M[i] + 0.6 * man_SD[i]):
+                really_grade.append(7)
+            elif (f8_sum_list[i] > man_M[i] + 0.6 * man_SD[i]) and (f8_sum_list[i] <= man_M[i] + man_SD[i]):
+                really_grade.append(8)
+            elif (f8_sum_list[i] > man_M[i] + man_SD[i]) and (f8_sum_list[i] <= man_M[i] + 1.5 * man_SD[i]):
+                really_grade.append(9)
+            elif (f8_sum_list[i] > man_M[i] + 1.5 * man_SD[i]):
+                really_grade.append(10)
+
+        return really_grade
+
+    # 打印分数
+    def print_grades(self,really_grade):
+        print("N =", really_grade[0])
+        print("E =", really_grade[1])
+        print("O =", really_grade[2])
+        print("A =", really_grade[3])
+        print("C =", really_grade[4])
+        print("N1 =", really_grade[5])
+        print("N2 =", really_grade[6])
+        print("N3 =", really_grade[7])
+        print("N4 =", really_grade[8])
+        print("N5 =", really_grade[9])
+        print("N6 =", really_grade[10])
+        print("E1 =", really_grade[11])
+        print("E2 =", really_grade[12])
+        print("E3 =", really_grade[13])
+        print("E4 =", really_grade[14])
+        print("E5 =", really_grade[15])
+        print("E6 =", really_grade[16])
+        print("O1 =", really_grade[17])
+        print("O2 =", really_grade[18])
+        print("O3 =", really_grade[19])
+        print("O4 =", really_grade[20])
+        print("O5 =", really_grade[21])
+        print("O6 =", really_grade[22])
+        print("A1 =", really_grade[23])
+        print("A2 =", really_grade[24])
+        print("A3 =", really_grade[25])
+        print("A4 =", really_grade[26])
+        print("A5 =", really_grade[27])
+        print("A6 =", really_grade[28])
+        print("C1 =", really_grade[29])
+        print("C2 =", really_grade[30])
+        print("C3 =", really_grade[31])
+        print("C4 =", really_grade[32])
+        print("C5 =", really_grade[33])
+        print("C6 =", really_grade[34])
 if __name__=='__main__':
-    gui_ins = GUI(0,{})
+    gui_ins = GUI(1)
     gui_ins.run()
+    file_re_index = open(r"./re-index", "r")
+    file_every_index = open(r"./all-index", "r")
+    score = Score_caculate()
+    score.alculate_reverse_score(file_re_index)
+    score_slip_sum_grades = score.slip_sum_grades(file_every_index)
+    score_add_all_grades = score.add_all_grades(score_slip_sum_grades)
+    score_calculation_really_grades=score.calculation_really_grades(score_add_all_grades)
+    #score.print_grades(score_calculation_really_grades)
+
+
+
+
 
 
--- README.md
+++ README.md
@@ -1,84 +1,21 @@
-# Python3中GUI图形界面构建
+# 大五性格测试GUI界面版本说明
 
-## 一.Tkinter
+## 本程序包含以下文件：
 
-### 1.Tkinter介绍
+### 1. question.txt
 
-介绍：Tkinter模块（Tk接口）是python的标准Tk GUI工具包的接口，通过内嵌在python解释器内部的Tcl解释器实现。先将Tkinter的调用转换成Tcl命令，然后交给Tcl解释器进行解释，实现python的GUI界面。
+里面包含了所有的问题，按顺序排序
 
-特点：（1）可用于windows/linux/unix/macintosh操作系统。
+### 2. re_index.txt
 
-​              （2）使用简单，灵活性高。
+包含需要反向计分的题目号
 
-​              （3）它是一个轻量级的跨平台图形用户界面开发工具。
+### 3. GUI.py
 
-### 2.Tkinter使用
+执行此程序，会出现如下界面：
 
-（1）创建窗口
+![捕获](C:\Users\Administrator\Desktop\捕获.PNG)
 
-```
-from tkinter import *
-window = Tk()
-window.title("性格测试")#窗口名
-window.geometry('600x200')#窗口大小,可以随意制定
-window.mainloop()
-```
+程序运行后显示的第一题，上面显示题目，下面点击选项进行选择，可以在回答当前题更改答案。第一题不可以点击上一题，如果上一题没有回答，但是点击下一题按钮，会有弹窗提示。回答240题后，关闭窗口，程序print最后得分。
 
-![1](/home/a/图片/1.png)
-
-（2）添加文本控件
-
-```
-from tkinter import *
-window = Tk()
-window.title("性格测试")#窗口名
-window.geometry('600x200')#窗口大小,可以随意制定
-
-#添加文本控件
-
-text=Text(window,width=60,height=4)#除了大小外，其他属性如背景颜色等见[网站](https://blog.csdn.net/weixin_42272768/article/details/100725243)。
-
-text.insert(INSERT,'Hellow world')
-
-text.pack()
-
-window.mainloop()
-```
-
-
-
-![2](/home/a/图片/2.png)
-
-
-
-（3）添加button控件
-
-```
-from tkinter import *
-window = Tk()
-window.title("性格测试")#窗口名
-window.geometry('600x200')#窗口大小,可以随意制定
-
-#添加文本控件
-
-text=Text(window,width=60,height=4)
-text.insert(INSERT,'hellow world')
-text.pack()
-
-def last_quesiton():
-    text.delete(0.0,END)
-    s1='last'
-    text.insert(INSERT,s1)
-
-last_question_button=Button(window,text="上一题",command=last_quesiton)
-last_question_button.pack()
-
-window.mainloop()
-```
-
-![3](/home/a/图片/3.png)
-
-点击上一题后：
-
-![2020-03-26 12-11-41 的屏幕截图](/home/a/图片/2020-03-26 12-11-41 的屏幕截图.png)
-
+此程序用面向对象进行编写，其中包含两个类，class GUI 定义了可视化界面的元件，和点击元件后触发的操作定义。class Score_caculate定义了分数计算的函数。此程序运行环境为python3.6及以上版本，linux和windows操作系统。
